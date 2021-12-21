fbbba58370b3352bb8b2cc01dd1db5a631de0738
Kato Shinya <kato.shinya.dev@gmail.com>
2021-12-21 17:09:38 +0900

    add

---
+++ py/02_home/first_word.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+'''
+You are given a string where you have to find its first word.
+
+When solving a task pay attention to the following points:
+
+There can be dots and commas in a string.
+A string can start with a letter or, for example, a dot or space.
+A word can contain an apostrophe and it's a part of a word.
+The whole text can be represented with one word and that's it.
+
+Input: A string.
+Output: A string.
+'''
+
+
+def first_word(text):
+    """
+        returns the first word in a given text.
+    """
+    word = ''
+    for token in text.strip():
+        if word and token in ' ,.':
+            break
+        elif token not in ' ,.':
+            word += token
+
+    return word
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(first_word("Hello world"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word(" a word ") == "a"
+    assert first_word("don't touch it") == "don't"
+    assert first_word("greetings, friends") == "greetings"
+    assert first_word("... and so on ...") == "and"
+    assert first_word("hi") == "hi"
+    assert first_word("Hello.World") == "Hello"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
